600ff68a45d93b95173a524a89ca04f49aae241b
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-10-06 15:10:55 +0300

    fix

--- src/utils/erros/validateObjectId.ts
+++ src/utils/erros/validateObjectId.ts
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 
 export function validateObjectId(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
-  
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({
-        error: 'Invalid Object ID',
+        error: `Invalid Object ID ${id}`,
       });
     }
   
